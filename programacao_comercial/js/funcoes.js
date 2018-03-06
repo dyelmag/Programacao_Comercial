38b02f5a753c1181f74d84fe3c9c262fdaaf1d7b
dyelmag <dyelmag@gmail.com>
2018-03-06 20:34:07 -0300

    Aula do dias 06/03

--- programacao_comercial/js/funcoes.js
+++ programacao_comercial/js/funcoes.js
@@ -1,1 +1,43 @@
-bee do
+function minhaFuncao1(){
+    console.log("asasas");
+    $('#area-01').css({
+        color: '#ff0000',
+        textTransform: 'uppercase',
+        width: '70%'
+    });
+};
+
+function minhaFuncao3(){
+    $('#area-02').empty();
+    var alunos = [
+        {
+            nome: "Aluno 01",
+            idade: 11
+        },
+        {
+            nome: "Aluno 02",
+            idade: 22
+        },
+        {
+            nome: "Aluno 03",
+            idade: 33
+        },
+        {
+            nome: "Aluno 04",
+            idade: 44
+        },
+        {
+            nome: "Aluno 05",
+            idade: 55
+        }
+    ];
+
+    for(i = 0 ; i < 5 ; i++){
+        console.log(alunos[i]);
+    }
+
+    var list = $('#area-02').append('<ul></ul>').find('ul');
+    $.each(alunos, function(index, value){
+        list.append('<li>' + value.nome + ' - ' + value.idade + '</li>');
+    });
+};
